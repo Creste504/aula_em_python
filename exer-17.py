0513d192e3314011a0c978b4f55499a30e253853
Creste504 <matheuschristian983@gmail.com>
2026-08-08 16:55:55 -0300

    mudanças leves

--- exer-17.py
+++ exer-17.py
@@ -1,8 +1,8 @@
 #Codigo para comparar dois numeros inteiros
 primeiro_valor = int (input('Digite o primeiro numero inteiro: '))
-segundo_valor = int (input('Digite o segunfo numero inteiro: '))
+segundo_valor = int (input('Digite o segundo numero inteiro: '))
 if primeiro_valor > segundo_valor:
-    print('O primeiro valor é maior')
+    print('O primeiro valor é maior.')
 elif primeiro_valor < segundo_valor:
     print('O segundo valor é maior.')
 elif primeiro_valor == segundo_valor:
