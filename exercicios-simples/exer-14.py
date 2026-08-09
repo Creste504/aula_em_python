9d86ca73fa443c215a10e34fdaf2a227f1105fe5
Creste504 <matheuschristian983@gmail.com>
2026-08-09 19:39:34 -0300

    ajeitando o antigo codigo dos triangulos

--- exercicios-simples/exer-14.py
+++ exercicios-simples/exer-14.py
@@ -2,7 +2,7 @@
 reta1 = float (input('Digite o valor da reta: '))
 reta2 = float (input('Digite o valor da reta: '))
 reta3 = float (input('Digite o valor da reta: '))
-if reta1 + reta2 > reta3 or reta3 + reta2 > reta1 or reta1 + reta3 > reta2:
+if reta1 + reta2 > reta3 and reta3 + reta2 > reta1 and reta1 + reta3 > reta2:
     print ('É possivel formar um triangulo.')
 else:
     print ('Não é possivel formar um triangulo.')
