ca1aa1dc87718347e2e040a667cc9852c32437eb
Creste504 <matheuschristian983@gmail.com>
2026-08-17 14:22:13 -0300

    Soma de numeros multiplos de 3 de 1 a 500.

---
+++ exercicios-simples/exer-27.py
@@ -0,0 +1,13 @@
+#Codigo pra fazer a soma dos numeros multiplos de 3 de 1 a 500
+lista = []
+for c in range (3, 500, 3):
+
+    print(c)
+
+    if c%2 == 1:
+        lista.append(c)
+
+soma_total = sum(lista)
+print ("A soma de todos os numero é igual a {}".format(soma_total))
+
+
