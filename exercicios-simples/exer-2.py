a5f40e40ec81f5b189ef1e41e81bb48274648731
Creste504 <matheuschristian983@gmail.com>
2026-08-30 11:53:36 -0300

    Correção do antigo codigo de ler um numero e destrinchar ele

--- exercicios-simples/exer-2.py
+++ exercicios-simples/exer-2.py
@@ -1,10 +1,31 @@
 #Codigo pra ler um numero e destrinchar ele
 numero = input ('Digite um numero de 0 a 9999: ')
-unidade = numero [3]
-dezena = numero [2:4]
-centena = numero [1:4]
-milhar = numero [0:4]
-print ("""Unidade: {}
+indice = (len(numero))
+if indice == 1:
+    unidade = numero [-1]
+elif indice == 2:
+    unidade = numero [-1]
+    dezena = numero [0:2]
+elif indice == 3:
+    unidade = numero [-1]
+    dezena = numero [1:3]
+    centena = numero [0:3]
+elif indice == 4:
+    unidade = numero [-1]
+    dezena = numero [2:4]
+    centena = numero [1:4]
+    milhar = numero [0:4]
+if indice == 1:
+    print ("Unidade: {}".format(unidade))
+elif indice == 2:
+    print ("""Unidade: {}
+Dezena : {} """.format(unidade, dezena))
+elif indice == 3:
+    print ("""Unidade: {}
+Dezena : {} 
+Centena: {} """.format(unidade, dezena, centena))
+elif indice == 4:       
+    print ("""Unidade: {}
 Dezena : {} 
 Centena: {} 
 milhar : {}""".format(unidade, dezena, centena, milhar))
